5ed0ae1541010b5ca86f063546e797b916288558
Nick Marus <nmarus@cognik.com>
2015-07-27 19:21:07 -0400

    Create repoclone.sh

---
+++ repoclone.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HELP_TEXT="useage: $0 <repo url>
+
+#Permissions
+USER=git
+GROUP=git
+
+#Reopository Path
+REPO=/repos
+
+#GIT executable
+GIT=`which git`
+
+#Args
+URL=$1
+NAME="$(echo "$(echo "$URL" | grep / | cut -d/ -f $(($(grep -o '/' <<< "$URL" | wc -l)+1)) -)")"
+GIT=$(sed)
+
+if [ $# != "2" ]; then
+  echo $HELP_TEXT
+  exit 1
+fi
+
+if [[ $NAME =~ \.git$ ]]; then
+  $GIT clone --bare --shared $URL $REPO/$NAME/
+  touch $REPO/$NAME/git-daemon-export-ok
+  chown $USER:$GROUP -R $REPO
+else
+  echo $HELP_TEXT
+  exit 1
+fi
