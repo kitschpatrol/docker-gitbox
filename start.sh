2d14fa59cb937a638383f175c9951f94916bc662
Nick Marus <nmarus@cognik.com>
2015-07-27 17:18:43 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,37 +1,59 @@
 #!/bin/bash
 dockername=gitbox
 
+set -e
+
+#description:   timestamp
+#usage:         echo ${timestamp}
+#returns:       timestamp
+
 timestamp() {
-  date +"%Y-%m-%d %T"
+        date +"%Y-%m-%d %T"
+}
+
+#description:   script logger
+#usage:         sclog <message> [<file>]
+#returns:       dated log message
+
+sclog() {
+        if [ ! -z ${2+x} ]; then
+                #if $2 is existing character file or not exisiting
+                if [ -c ${2} ] || [ ! -a ${2} ]; then
+                        echo "$(timestamp): ${1}" | tee -a ${2}
+                else
+                        echo "$(timestamp): ${1}"
+                fi
+        else
+                echo "$(timestamp): ${1}"
+        fi
 }
 
 startc() {
-  echo "$(timestamp): Services for ($dockername) are being started..."
+  sclog "Services for ($dockername) are being started..."
   /etc/init.d/php5-fpm start > /dev/null
   /etc/init.d/fcgiwrap start > /dev/null
   /etc/init.d/nginx start > /dev/null
   /etc/init.d/gitd start > /dev/null
-  echo "$(timestamp): The ($dockername) services have started..."  
+  sclog "The ($dockername) services have started..."  
 }
 
 stopc() {
-  echo "$(timestamp): Services for ($dockername) are being stopped..."
+  sclog "Services for ($dockername) are being stopped..."
   /etc/init.d/gitd stop > /dev/null
   /etc/init.d/nginx stop > /dev/null
   /etc/init.d/php5-fpm stop > /dev/null
   /etc/init.d/fcgiwrap stop > /dev/null
-  echo "$(timestamp): Services for ($dockername) have successfully stopped..."
-  echo "$(timestamp): Exiting..."
+  sclog "Services for ($dockername) have successfully stopped. Exiting."
 }
 
 #trap "docker stop <container>"
 trap "(stopc)" TERM
 
 #startup
-echo "$(timestamp): Container ($dockername) is starting..."
+sclog  "Container ($dockername) is starting..."
 
 #create repo main folder
-echo "$(timestamp): Checking if "repos" folder exists..."
+sclog  "Checking if "repos" folder exists..."
 if [ ! -e /repos ]; then
   mkdir /repos
 fi
@@ -43,8 +65,14 @@ chown git:git -R 777 /repos
 startc
 
 #pause script to keep container running...
-echo "$(timestamp): [Hit enter key to stop] or run 'docker stop $dockername'"
-read
+sclog "Services for container successfully started."
+stop="no"
+while [ "$stop" == "no" ]
+do
+sclog "Type [stop] or run 'docker stop $dockername' from host."
+read input
+if [ "$input" == "stop" ]; then stop="yes"; fi
+done
 
 #stop services
 stopc
