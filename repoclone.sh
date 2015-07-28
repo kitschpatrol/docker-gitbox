29ac77bfaa7a7be55fe104c82d9cf5de0bf9b683
Nick Marus <nmarus@cognik.com>
2015-07-28 02:08:24 -0400

    Update repoclone.sh

--- repoclone.sh
+++ repoclone.sh
@@ -12,14 +12,14 @@ REPO="/repos"
 #GIT executable
 GIT="`which git`"
 
-#Args
-URL=$1
-NAME="$(echo "$(echo "$URL" | grep / | cut -d/ -f $(($(grep -o '/' <<< "$URL" | wc -l)+1)) -)")"
-
 if (( $# > 2 )); then
   echo $HELP_TEXT
   exit 1
 fi
+
+#Args
+URL=$1
+NAME="$(echo "$(echo "$URL" | grep / | cut -d/ -f $(($(grep -o '/' <<< "$URL" | wc -l)+1)) -)")"
 
 if [[ $NAME =~ \.git$ ]]; then
   $GIT clone --bare --shared $URL $REPO/$NAME/
